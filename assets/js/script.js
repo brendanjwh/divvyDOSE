58386fab50e1c08aa645f955c2fbd28ff9941010
Brendan White <eggrollcontrol@gmail.com>
2017-10-27 14:34:13 -0500

    show location by address implemented and working

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,6 +61,91 @@ if (hasFavoriteLocations) {
   favoriteLocationsListGroup.show();
 }
 
+// This function presents the address which was entered in the text field in the map
+
+function showLocationByAddress(e) {
+  e.preventDefault();
+
+  // Getting the coordinates of the entered address
+
+  GMaps.geocode({
+    address: $("#address")
+      .val()
+      .trim(),
+    callback: function(results, status) {
+      if (status !== "OK") return;
+
+      var latlng = results[0].geometry.location,
+        fullAddress = results[0].formatted_address,
+        isLocationFavorite = false,
+        locationsArray = JSON.parse(localStorage.getItem("favorite-locations")),
+        saveLocation = $("#save-location");
+
+      var map = new GMaps({
+        el: "#map",
+        lat: latlng.lat(),
+        lng: latlng.lng()
+      });
+
+      // Adding a marker on the wanted location
+
+      map.addMarker({
+        lat: latlng.lat(),
+        lng: latlng.lng()
+      });
+
+      // Checking if this address exists in the favorites array
+
+      if (locationsArray.length) {
+        locationsArray.forEach(function(item) {
+          if (item.lat == latlng.lat() && item.lng == latlng.lng()) {
+            isLocationFavorite = true;
+          }
+        });
+      }
+
+      // Adding the address to the html and setting data attributes with the coordinates
+      saveLocation
+        .text(fullAddress)
+        .attr({ "data-lat": latlng.lat(), "data-lng": latlng.lng() });
+
+      // Removing the active class from all favorite locations
+      favoriteLocationsListGroup
+        .find("a.list-group-item")
+        .removeClass("active-location");
+
+      // Changing the icon to become non-favorite
+
+      if (!isLocationFavorite) {
+        favoriteIcon
+          .removeClass("glyphicon-star")
+          .addClass("glyphicon-star-empty");
+      } else {
+        // Adding the active class and add the favorite icon on the given favorite location
+        favoriteIcon
+          .removeClass("glyphicon-star-empty")
+          .addClass("glyphicon-star");
+
+        // Find the entry in the favorite locations list that corresponds
+        // to the current location, and mark it as active.
+
+        favoriteLocationsListGroup
+          .find(
+            'a.list-group-item[data-lat="' +
+              latlng.lat() +
+              '"][data-lng="' +
+              latlng.lng() +
+              '"]'
+          )
+          .addClass("active-location");
+      }
+
+      // Show the html of the given location
+      saveContainer.show();
+    }
+  });
+}
+
 // This function saves a location to favorites and adds it to localStorage
 
 function saveFavoriteLocation(e) {
