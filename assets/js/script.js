dbcfb8eaca9cae2992e34b3153bae7c49fd8d5be
Brendan White <eggrollcontrol@gmail.com>
2017-10-29 17:12:04 -0500

    removing gmap methods for standard google maps api methods.  mixing the both has been fruitless

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,223 +1,132 @@
-function allLetter(inputtxt) {
-  var letters = /^[A-Za-z]+$/;
-  if (inputtxt.value.match(letters)) {
-    return true;
-  } else {
-    alert("Name must be all letters...");
-    return false;
-  }
-}
-
-var saveContainer = $(".save-container"),
-  favoriteIcon = saveContainer.find(".glyphicon"),
-  favoriteLocationsListGroup = $(".list-group");
-
-var hasFavoriteLocations = false;
-
 // Initialize a google maps using the gmaps library.
-var map = new GMaps({
-  el: "#map",
-  lat: "0",
-  lng: "0",
-  zoom: 1
-});
+// var map = new GMaps({
+//   el: "#map",
+//   lat: "0",
+//   lng: "0",
+//   zoom: 1
+// });
 
 // Initialize the favorite locations array which is kept in localStorage
 
-if (!localStorage.hasOwnProperty("favorite-locations")) {
-  localStorage.setItem("favorite-locations", JSON.stringify([]));
-}
+// if (!localStorage.hasOwnProperty("favorite-locations")) {
+//   localStorage.setItem("favorite-locations", JSON.stringify([]));
+// }
 
-hasFavoriteLocations = JSON.parse(localStorage.getItem("favorite-locations"))
-  .length
-  ? true
-  : false;
+// hasFavoriteLocations = JSON.parse(localStorage.getItem("favorite-locations"))
+//   .length
+//   ? true
+//   : false;
+var initialLocation = $("#address")
+  .val()
+  .trim();
+
+var theLocation;
+var map;
 
 // Form submit and Search icon handlers
 $(".glyphicon-search").click(showLocationByAddress);
 $("#geocoding_form").submit(showLocationByAddress);
 
-// Click handler on any of the favorite locations
-$(document).on("click", "a.list-group-item", showLocationByCoordinates);
-
-// Click handler on the favorite(star) icon to become saved or removed
-$(document).on("click", ".glyphicon-star", removeFavoriteLocation);
-$(document).on("click", ".glyphicon-star-empty", saveFavoriteLocation);
-
-if (hasFavoriteLocations) {
-  var array = JSON.parse(localStorage.getItem("favorite-locations"));
-
-  favoriteLocationsListGroup.empty();
-  favoriteLocationsListGroup.append(
-    '<span class="list-group-item active">Saved Locations</span>'
-  );
-
-  array.forEach(function(item) {
-    favoriteLocationsListGroup.append(
-      '<a class="list-group-item" data-lat="' +
-        item.lat +
-        '" data-lng="' +
-        item.lng +
-        '" data-createdAt="' +
-        item.createdAt +
-        '">' +
-        item.address +
-        '<span class="createdAt">' +
-        moment(item.createdAt).fromNow() +
-        '</span><span class="glyphicon glyphicon-menu-right"></span></a>'
-    );
-  });
-
-  favoriteLocationsListGroup.show();
-}
-
-// This function presents the address which was entered in the text field in the map
-
 function showLocationByAddress(e) {
+  console.log("showlocationbyaddress called");
   e.preventDefault();
 
   // Getting the coordinates of the entered address
-
   GMaps.geocode({
     address: $("#address")
       .val()
       .trim(),
-    callback: function(results, status) {
-      if (status !== "OK") return;
+    callback: function(center, status) {
+      console.log("callback function started");
+      if (status !== "OK") {
+        console.log("status !== ok");
+        return;
+      }
+      console.log("status =" + status);
 
-      var latlng = results[0].geometry.location,
-        fullAddress = results[0].formatted_address,
-        isLocationFavorite = false,
-        locationsArray = JSON.parse(localStorage.getItem("favorite-locations")),
-        saveLocation = $("#save-location");
-
-      var map = new GMaps({
-        el: "#map",
-        lat: latlng.lat(),
-        lng: latlng.lng()
-      });
-
-      // Adding a marker on the wanted location
-
-      map.addMarker({
-        lat: latlng.lat(),
-        lng: latlng.lng()
-      });
-
-      // Checking if this address exists in the favorites array
-
-      if (locationsArray.length) {
-        locationsArray.forEach(function(item) {
-          if (item.lat == latlng.lat() && item.lng == latlng.lng()) {
-            isLocationFavorite = true;
-          }
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        console.log("in place services");
+        var latlng = center[0].geometry.location;
+        initialLocation = latlng;
+        var fullAddress = center[0].formatted_address;
+        map = new GMaps({
+          el: "#map",
+          lat: latlng.lat(),
+          lng: latlng.lng()
         });
+        console.log("map: " + map);
+        getPharmacies(map, initialLocation);
       }
-
-      // Adding the address to the html and setting data attributes with the coordinates
-      saveLocation
-        .text(fullAddress)
-        .attr({ "data-lat": latlng.lat(), "data-lng": latlng.lng() });
-
-      // Removing the active class from all favorite locations
-      favoriteLocationsListGroup
-        .find("a.list-group-item")
-        .removeClass("active-location");
-
-      // Changing the icon to become non-favorite
-
-      if (!isLocationFavorite) {
-        favoriteIcon
-          .removeClass("glyphicon-star")
-          .addClass("glyphicon-star-empty");
-      } else {
-        // Adding the active class and add the favorite icon on the given favorite location
-        favoriteIcon
-          .removeClass("glyphicon-star-empty")
-          .addClass("glyphicon-star");
-
-        // Find the entry in the favorite locations list that corresponds
-        // to the current location, and mark it as active.
-
-        favoriteLocationsListGroup
-          .find(
-            'a.list-group-item[data-lat="' +
-              latlng.lat() +
-              '"][data-lng="' +
-              latlng.lng() +
-              '"]'
-          )
-          .addClass("active-location");
-      }
-
-      // Show the html of the given location
-      saveContainer.show();
+      // end of callback()
     }
   });
 }
 
-// This function saves a location to favorites and adds it to localStorage
+function callback2(results, status) {
+  console.log("results in callback2: " + results);
+  console.log("results in callback2: " + results.length);
+  for (var i = 0; i < results.length; i++) {
+    console.log("result location: " + results[i].geometry.location);
+    console.log("marker created");
+    createMarker(results[i]);
+  }
+}
 
-function saveFavoriteLocation(e) {
-  e.preventDefault();
-
-  var saveLocation = $("#save-location"),
-    locationAddress = saveLocation.text(),
-    isLocationFavorite = false,
-    locationsArray = JSON.parse(localStorage.getItem("favorite-locations"));
-
-  var location = {
-    lat: saveLocation.attr("data-lat"),
-    lng: saveLocation.attr("data-lng"),
-    createdAt: moment().format()
+function getPharmacies(map, initialLocation) {
+  console.log("inside getPharmacies");
+  var request = {
+    location: initialLocation,
+    // 10 miles = 16,093 meters
+    radius: 16093.4,
+    type: "pharmacy"
   };
+  console.log("map: " + map);
+  var service = new google.maps.places.PlacesService(map);
+  //service.nearbySearch(request, callback2);
 
-  // Checking if this location is in the favorites array
+  // var map = new GMaps({
+  //   el: "#map",
+  //   lat: latlng.lat(),
+  //   lng: latlng.lng()
+  // });
+  // console.log("map reassigned: " + map);
 
-  if (locationsArray.length) {
-    locationsArray.forEach(function(item) {
-      if (item.lat == location.lat && item.lng == location.lng) {
-        isLocationFavorite = true;
-      }
-    });
-  }
+  // Adding a marker on the wanted location
+  // map.addMarker({
+  //   lat: latlng.lat(),
+  //   lng: latlng.lng()
+  // });
+  // end Gmaps.geocode
+}
 
-  // If the given location is not in favorites,
-  // add it to the HTML and to localStorage's array
+// var infowindow;
 
-  if (!isLocationFavorite) {
-    favoriteLocationsListGroup.append(
-      '<a class="list-group-item active-location" data-lat="' +
-        location.lat +
-        '" data-lng="' +
-        location.lng +
-        '" data-createdAt="' +
-        location.createdAt +
-        '">' +
-        locationAddress +
-        '<span class="createdAt">' +
-        moment(location.createdAt).fromNow() +
-        "</span>" +
-        '<span class="glyphicon glyphicon-menu-right"></span>' +
-        "</span></a>"
-    );
+// function initMap() {
+//   var location = { lat: 0, lng: 0 };
 
-    favoriteLocationsListGroup.show();
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: location,
+//     zoom: 1
+//   });
+//   infowindow = new google.maps.InfoWindow();
+// }
 
-    // Adding the given location to the localStorage's array
-    locationsArray.push({
-      address: locationAddress,
-      lat: location.lat,
-      lng: location.lng,
-      createdAt: moment().format()
-    });
+function createMarker(place) {
+  //console.log("place: " + place);
+  //console.log("in create marker");
+  var placeLoc = place.geometry.location;
+  console.log("placeloc = " + placeLoc);
+  // var marker = new google.maps.Marker({
+  //   map: map,
+  //   position: placeLoc
+  // });
+  map.addMarker({
+    position: placeLoc
+  });
+  //console.log("marker: " + marker);
 
-    localStorage.setItem("favorite-locations", JSON.stringify(locationsArray));
-
-    // Make the star icon full, to signify that this location is now favorite
-    favoriteIcon.removeClass("glyphicon-star-empty").addClass("glyphicon-star");
-
-    // Now we have at least one favorite location
-    hasFavoriteLocations = true;
-  }
+  // google.maps.event.addListener(marker, "click", function() {
+  //   infowindow.setContent(place.name);
+  //   infowindow.open(map, this);
+  // });
 }
